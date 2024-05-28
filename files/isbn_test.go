33636b329ee802713c742017248369e449bf47d3
uWpDLW <uWpDLW@cyber-dojo.org>
2024-05-28 09:28:53 +0000

    4 ran tests, predicted red, got red

--- files/isbn_test.go
+++ files/isbn_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestCleanUp(t *testing.T) {
     assert.Equal(t, "0470845252", cleanUp("0-470-84525-2"))
+}
 
 func TestVefify(t *testing.T) {
     assert.True(t, Verify("0-321-14653-0"))

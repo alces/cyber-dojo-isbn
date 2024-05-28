db1f18bfe684b5d1727806e16c8c13d34ed57281
uWpDLW <uWpDLW@cyber-dojo.org>
2024-05-28 09:28:34 +0000

    3 ran tests, predicted red, got red

--- files/isbn.go
+++ files/isbn.go
@@ -3,3 +3,7 @@ package isbn
 func Verify(isbn string) bool {
     return len(isbn) >= 10
 }
+
+func cleanUp(isbn string) string {
+    return ""
+}
--- files/isbn_test.go
+++ files/isbn_test.go
@@ -5,6 +5,9 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
+func TestCleanUp(t *testing.T) {
+    assert.Equal(t, "0470845252", cleanUp("0-470-84525-2"))
+
 func TestVefify(t *testing.T) {
     assert.True(t, Verify("0-321-14653-0"))
 }

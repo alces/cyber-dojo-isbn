4806c47d05046d28ef17c279128f2ddbe15d834c
uWpDLW <uWpDLW@cyber-dojo.org>
2024-05-29 07:18:08 +0000

    45 ran tests, predicted red, got red

--- files/checksum.go
+++ files/checksum.go
@@ -1,9 +1,14 @@
 package isbn
 
+import (
+    "fmt"
+)
+
 func isbn10Checksum(numbers []byte) (result byte) {    
     for i := 0; i < 9; i ++ {
         result += numbers[i] * byte(i + 1)
     }
+    fmt.Printf("result = %v", result)
     
     return result % 11
 }    

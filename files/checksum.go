f6db79a502400acaae6c3bfabb2a537aae7eacf6
uWpDLW <uWpDLW@cyber-dojo.org>
2024-05-29 07:14:27 +0000

    44 ran tests, predicted green, got red

--- files/checksum.go
+++ files/checksum.go
@@ -2,7 +2,7 @@ package isbn
 
 func isbn10Checksum(numbers []byte) (result byte) {    
     for i := 0; i < 9; i ++ {
-        result += numbers[i] * (i + 1)
+        result += numbers[i] * byte(i + 1)
     }
     
     return result % 11

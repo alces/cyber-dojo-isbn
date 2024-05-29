699b1428376756633c895e8c93c11c21e88bbaaf
uWpDLW <uWpDLW@cyber-dojo.org>
2024-05-29 07:38:57 +0000

    55 ran tests, predicted green, got green

--- files/checksum.go
+++ files/checksum.go
@@ -23,5 +23,5 @@ func isbn13Checksum(numbers []byte) byte {
         result += int(numbers[i]) * coef           
     }
     
-    return byte((10 - (result % 10)) % 10)
+    return byte(10 - (result % 10))
 }

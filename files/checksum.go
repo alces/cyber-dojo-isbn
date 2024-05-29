3e42e7d223bbb08985ad9d27d73aa0801e808a7a
uWpDLW <uWpDLW@cyber-dojo.org>
2024-05-29 07:20:09 +0000

    48 ran tests, predicted green, got green

--- files/checksum.go
+++ files/checksum.go
@@ -1,14 +1,10 @@
 package isbn
 
-import (
-    "fmt"
-)
-
 func isbn10Checksum(numbers []byte) byte {
     var result int
     
     for i := 0; i < 9; i ++ {
-        result += numbers[i] * (i + 1)
+        result += int(numbers[i]) * (i + 1)
     }
     
     return byte(result % 11)

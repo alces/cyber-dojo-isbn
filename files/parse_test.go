e1817842b4bd138f2ec9831f59aa24ff5158bf2d
uWpDLW <uWpDLW@cyber-dojo.org>
2024-05-30 06:56:50 +0000

    57 ran tests, predicted green, got green

---
+++ files/parse_test.go
@@ -0,0 +1,69 @@
+package isbn
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+var cleanUpTestResults = []struct{
+    argument string
+    expected string
+} {
+    {"0470845252", "0470845252"},
+    {"0-470-84525-2", "0470845252"},
+    {"0 470 84525 2", "0470845252"},
+}
+
+func TestCleanUp(t *testing.T) {
+    for _, r := range cleanUpTestResults {
+        assert.Equal(t, r.expected, cleanUp(r.argument), r.argument)
+    }
+}
+
+var isISBN10TestResults = []struct {
+    argument string
+    expected bool
+} {
+    {"0470845252", true},
+    {"470845252", false},
+    {"47084525211", false},
+    {"847084525X", true},
+    {"X470845258", false},
+    {"47084525X", false},
+    {"4700840525X", false},
+}
+
+func TestIsISBN10(t *testing.T) {
+    for _, r := range isISBN10TestResults {
+        assert.Equal(t, r.expected, isISBN10(r.argument), r.argument)
+    }
+}
+
+var isISBN13TestResults = []struct {
+    argument string
+    expected bool
+} {
+    {"9780470059029", true},
+    {"978047005902", false},
+    {"97804700590293", false},
+}
+
+func TestIsISBN13(t *testing.T) {
+    for _, r := range isISBN13TestResults {
+        assert.Equal(t, r.expected, isISBN13(r.argument), r.argument)
+    }
+}
+
+var stringToNumbersTestResults = []struct {
+    argument string
+    expected []byte
+} {
+    {"0470845252", []byte{0, 4, 7, 0, 8, 4, 5, 2, 5, 2}},
+    {"847084525X", []byte{8, 4, 7, 0, 8, 4, 5, 2, 5, 10}},
+}
+
+func TestStringToNumbers(t *testing.T) {
+    for _, r := range stringToNumbersTestResults {
+        assert.Equal(t, r.expected, stringToNumbers(r.argument))
+    }
+}
